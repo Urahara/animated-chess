eaa9cfaadb6cdb43c2ab31cac3547f85863d11b8
Urahara <Urahara@users.noreply.github.com>
2025-05-15 22:04:41 -0300

    Update contex.ts

--- src/context/ChessboardContext/contex.ts
+++ src/context/ChessboardContext/contex.ts
@@ -3,7 +3,7 @@ import { ChessboardContextProps } from "./types";
 import { defaultPiecesInfo } from "@/components";
 
 export const ChessboardContext = createContext<ChessboardContextProps>({
-  selectedPieceCoords: { alive: true, color: 'white', coords: {x: null, y: null}, id:'', type:"pawn", firstMove: true, rotate: 0},
+  selectedPieceCoords: { alive: true, color: 'white', coords: {x: null, y: null}, id:'', type:"pawn", firstMove: true},
   setSelectedPieceCoords: () => {},
   convertToChessCoords: () => "",
   convertFromChessCoords: () => ({ x: 0, y: 0 }),
